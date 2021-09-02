525758e5f847b25cec5713ba729d1ba7f92ea18e
fkashrafi <mfahadkhanashrafi@outlook.com>
2021-09-02 19:46:23 +0500

    image path constant

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -13,6 +13,8 @@ import {
 import { colors } from '../../constants/theme';
 import Link from 'next/link';
 
+const coverImage = 'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80';
+
 export default function login() {
     return (
         <Stack minH={'100vh'}
@@ -69,9 +71,7 @@ export default function login() {
                 <Image
                     alt={'Login Image'}
                     objectFit={'cover'}
-                    src={
-                        'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-                    }
+                    src={`${coverImage}`}
                 />
             </Flex>
         </Stack>
